920e1921c69db5954b8bbb7c8f3711af4dfeb5d2
VGajdosova <ver.gajdosova@gmail.com>
2016-11-17 20:55:45 +0100

    updating

--- plot2.R
+++ plot2.R
@@ -8,7 +8,7 @@ Date_subset <- Data[Data$Date %in% c("1/2/2007","2/2/2007") ,]
 #Global active power -> conversion to numeric
 Global_active_power_num <- as.numeric(Date_subset$Global_active_power)
 
-#str(subSetData)
+#Date_time conversion
 Date_time <- strptime(paste(Date_subset$Date, Date_subset$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 
 #Plotting to png
